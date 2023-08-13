6ae17e390446dfd02fccabef9c3164c0c6f3ea7d
abhishekgoud343 <127048012+abhishekgoud343@users.noreply.github.com>
2023-08-14 01:03:14 +0530

    Update NoteEditor.java

--- Note Editor/src/NoteEditor.java
+++ Note Editor/src/NoteEditor.java
@@ -9,6 +9,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class NoteEditor implements ActionListener {
     JFrame frame;
     Dimension size;
